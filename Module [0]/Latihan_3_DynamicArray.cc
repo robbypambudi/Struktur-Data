6cbb8aff8df34b982f67cedc5e0b763b6a7f7e02
Robby Pambudi <robby.pambudi10@gmail.com>
2022-02-25 23:45:38 +0700

    Latihan 3 Dynamic Array

---
+++ Module [0]/Latihan_3_DynamicArray.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct dynamicArray_t
+{
+    char *data;
+    int size;
+    int capacity;
+} dynamicArray;
+
+void init(dynamicArray *array);
+void getString(dynamicArray *array);
+void printString(dynamicArray *array);
+
+void init(dynamicArray *array)
+{
+    array->data = NULL;
+    array->size = 0;
+    array->capacity = 0;
+}
+
+void getString(dynamicArray *array)
+{
+    char input[100];
+    cin.getline(input, 100);
+    int length = strlen(input);
+    if (array->capacity < length)
+    {
+        array->capacity = length;
+        array->data = (char *)realloc(array->data, array->capacity * sizeof(char));
+    }
+    for (int i = 0; i < length; i++)
+    {
+        array->data[i] = input[i];
+    }
+    array->size = length;
+}
+void printString(dynamicArray *array)
+{
+    for (int i = 0; i < array->size; i++)
+    {
+        cout << array->data[i];
+    }
+}
+int main()
+{
+    dynamicArray array;
+    init(&array);
+    getString(&array);
+    printString(&array);
+    return 0;
+}
