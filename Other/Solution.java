0c91a3a656242941f643d5ca19ac46d8f3ca6df9
Robby Pambudi <robby.pambudi10@gmail.com>
2022-03-04 12:57:34 +0700

    Add Other

---
+++ Other/Solution.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+    
+    static class Gerbong {
+        String nama;
+        Gerbong next;
+
+        Gerbong(){
+        }
+        void setGerbong(String nama){
+            this.nama = nama;
+        }
+        String getGerbong(){
+            return nama;
+        }
+        void setNext(Gerbong next){
+            this.next = next;
+        }
+        Gerbong getNext(){
+            return next;
+        }
+    }
+    static class Kereta{
+        String NamaKereta;
+        Gerbong kontainer;
+        
+        Kereta(){
+            kontainer = new Gerbong();
+        }
+        Gerbong getGerbong(){
+            return kontainer;
+        }
+        
+        void setNamaKereta(String nama){
+            this.NamaKereta = nama;
+        }
+        String getNamaKereta(){
+            return NamaKereta;
+        }
+        
+    }
+
+    
+
+    public static void pushKereta(Kereta baru, String nama, String [] gerbong){
+        baru.setNamaKereta(nama);
+        Gerbong g = baru.getGerbong();
+
+        for(int i = 0; i < gerbong.length; i++){
+            Gerbong temp = new Gerbong();
+            if (i == 0){
+                g.setGerbong(gerbong[i]);
+                g.setNext(temp);
+                g = g.getNext();
+            }
+            else {
+                g.setGerbong(gerbong[i]);
+                g.setNext(temp);
+                g = g.getNext();
+            }            
+        }
+
+    } 
+
+    public static void printKereta(Kereta k){
+        Gerbong g = k.getGerbong();
+        System.out.println(k.getNamaKereta());
+        while(g != null){
+            if(g.next.getGerbong() == null){
+                System.out.print(g.getGerbong());
+                break;
+            }
+            System.out.print(g.getGerbong()+"-");
+            g = g.next;
+        }
+        
+    }
+        
+    
+
+    public static void main(String[] args){
+        Kereta kereta = new Kereta();
+
+        Scanner in = new Scanner(System.in);
+        
+        String NamaKereta = in.nextLine();
+        String Gerbong = in.nextLine();
+
+        // Split Gerbong
+        String[] arrGerbong = Gerbong.split("-");
+        pushKereta(kereta,NamaKereta, arrGerbong);
+        printKereta(kereta);
+
+    }
+
+}
